911266ff334dbc0dbcfb4400e1ca84611fd4f4b4
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2026-01-31 21:52:28 +0000

    feat: extract h1 to new component called YearTitle

--- src/components/custom/ContentArea/ContentArea.tsx
+++ src/components/custom/ContentArea/ContentArea.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import { useBackground } from "../../../contexts/BackgroundContext";
 import BulletList from "./BulletList";
-import { ContentList } from "./ContentList";
+import ContentList from "./ContentList";
+import YearTitle from "./YearTitle";
 
 export default function ContentArea() {
-  const { selectedYear } = useBackground();
 
   return (
     <div className="content-area flex flex-col self-start md:pl-6 max-h-[290px] h-[290px]">
-      <h1 className="text-3xl pb-4 mb-6 sticky top-0 z-10 bg-white shadow-xs">
-        {selectedYear}
-      </h1>
+      <YearTitle />
 
       <div className="content-view flex justify-between max-h-[230px] h-[230px]">
         <ContentList />
