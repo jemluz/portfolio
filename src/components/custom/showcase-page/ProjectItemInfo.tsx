0bcf51722254b33c8b42654c716d6741c64d78c8
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2026-06-20 20:13:48 -0300

    feat: create `ProjectItemInfo` component

---
+++ src/components/custom/showcase-page/ProjectItemInfo.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import LanguageStackList from "./LanguageStackList";
+import ProjectInfoButtons from "./ProjectInfoButtons";
+import { ProjectItemInfoProps } from "./showcase.types";
+import { courstardSans } from "@/lib/fonts";
+
+export default function ProjectItemInfo({
+  title,
+  slug,
+  category,
+  description,
+  languageStack,
+  className,
+  webLink,
+}: ProjectItemInfoProps) {
+  const hasLanguageStack = languageStack && languageStack.length > 0;
+
+  return (
+    <section
+      className={cn("project-item-info", className)}
+      aria-label={`Detalhes do projeto ${title}`}
+    >
+      <h3 className="text-3xl font-bold mb-6 tracking-tight text-gray-900 flex items-center gap-3 flex-wrap">
+        {title}
+        <span className="text-gray-400 font-medium tracking-normal text-2xl">
+          [{category}]
+        </span>
+      </h3>
+
+      <p
+        className={cn(
+          "text-gray-500 mb-8 max-w-lg leading-[1.8] whitespace-pre-line text-[15px]",
+          courstardSans.className,
+        )}
+      >
+        {description}
+      </p>
+
+      {hasLanguageStack && <LanguageStackList languageStack={languageStack} />}
+
+      <ProjectInfoButtons slug={slug} webLink={webLink} />
+    </section>
+  );
+}
