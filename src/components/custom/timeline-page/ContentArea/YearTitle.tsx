404223b78c9ecd3b9b3f1eee720695422d38c8c6
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2026-06-20 22:17:36 -0300

    refactor: remove unecessary `use client`

--- src/components/custom/timeline-page/ContentArea/YearTitle.tsx
+++ src/components/custom/timeline-page/ContentArea/YearTitle.tsx
@@ -1,16 +1,14 @@
 import { useTimelinePage } from "@/contexts/TimelinePageContext";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { cn } from "@/lib/utils";
 
 export default function YearTitle() {
   const { selectedYear } = useTimelinePage();
-  const isDesktop = useMediaQuery("(min-width: 769px)");
 
   return (
     <h1
       className={cn(
         "year-title flex items-center text-3xl  sticky top-0 z-10 bg-white ",
-        !isDesktop ? "pl-2" : "pb-4 mb-6 shadow-xs",
+        "pl-2 md:pl-0 md:pb-4 md:mb-6 md:shadow-xs",
       )}
     >
       {selectedYear}
