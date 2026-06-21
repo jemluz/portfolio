bcd7f4e7d9b5240f0cd3968a83edbb31dd778ca8
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2026-06-21 18:50:50 -0300

    fix(I132_S135): import of `Menu` component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { suseMono } from "@/lib/fonts";
 import { headers } from "next/headers";
-import { Menu } from "@/components/custom/common/Menu";
 import BinaryArt from "@/components/custom/common/BinaryArt";
+import { Menu } from "@/components/custom/common/Menu";
 
 export const metadata: Metadata = {
   title: "Jemluz ",
