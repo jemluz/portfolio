693b726bf7227a10f8df368a7e4ce59249217b44
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2026-06-27 16:07:28 -0300

    feat: setup internationalization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { suseMono } from "@/lib/fonts";
 import { headers } from "next/headers";
 import Menu from "@/components/custom/common/Menu";
 import BinaryArt from "@/components/custom/common/BinaryArt";
+import { NextIntlClientProvider } from "next-intl";
 
 export const metadata: Metadata = {
   title: "Jemluz ",
@@ -30,9 +31,11 @@ export default async function RootLayout({
           enableSystem={false}
         >
           {/* <ThemeToggle/> */}
-          <Menu />
-          <BinaryArt />
-          {children}
+          <NextIntlClientProvider>
+            <Menu />
+            <BinaryArt />
+            {children}
+          </NextIntlClientProvider>
         </ThemeProvider>
       </body>
     </html>
--- src/components/custom/common/Menu.tsx
+++ src/components/custom/common/Menu.tsx
@@ -8,6 +8,15 @@ import {
 } from "@/components/ui/navigation-menu";
 import MenuItem from "./Menu/MenuItem";
 import ActiveLink from "./Menu/ActiveLink";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useLocale } from "next-intl";
 
 export default function Menu() {
   const { pathname, activeLink, subpathSegments, inactiveLinks } =
@@ -16,10 +25,20 @@ export default function Menu() {
   const shouldRenderShowcaseBreadcrumb =
     activeLink?.href === "/showcase" && subpathSegments.length > 0;
 
+  const locale = useLocale();
+
+  const languages = [
+    { code: " en", label: "🇺🇸 EN" },
+    { code: " pt-BR", label: "🇧🇷 PT-BR" },
+  ];
+
   return (
     <header className="w-screen bg-white/80 flex  justify-between lg:px-12 transition-all z-50">
-      <NavigationMenu viewport={false} className="mx-auto w-full max-w-none">
-        <NavigationMenuList className="mx-auto flex items-center gap-4 py-4">
+      <NavigationMenu
+        viewport={false}
+        className="flex items-center mx-auto w-full max-w-none"
+      >
+        <NavigationMenuList className="mr-4 flex items-center gap-4 py-4">
           <NavigationMenuItem className="inactive-links mr-2 flex items-center gap-8">
             {inactiveLinks.map(({ href, icon: Icon, label }) => {
               return (
@@ -42,6 +61,23 @@ export default function Menu() {
             />
           </NavigationMenuItem>
         </NavigationMenuList>
+
+        <Select defaultValue={languages[1].code}>
+          <SelectTrigger className="w-[120px] border-0 shadow-none">
+            <SelectValue placeholder={locale} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {languages.map(({ code, label }) => {
+                return (
+                  <SelectItem key={code} value={code}>
+                    {label}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </NavigationMenu>
     </header>
   );
