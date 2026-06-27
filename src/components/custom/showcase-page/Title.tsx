6171dd39fc9c2b1cd6378bd136d9b886c5a7a43b
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2026-06-27 11:31:42 -0300

    feat: create `Title` component for `showcase` page

---
+++ src/components/custom/showcase-page/Title.tsx
@@ -0,0 +1,10 @@
+export default function Title() {
+  return (
+    <section className="flex flex-col gap-4">
+      <h1 className="text-4xl md:text-5xl font-bold text-gray-900">Showcase</h1>
+      <p className="text-md lg:text-lg text-gray-400">
+        Lets take a look at some of the projects Ive worked on.
+      </p>
+    </section>
+  );
+}
